08c24bb25fbde948a99ace1bd25f799ab7e8216f
PauloDev13 <devpgm2020@gmail.com>
2022-09-17 13:00:04 -0300

    Create project spring

---
+++ pagination-backend/src/test/java/br/com/devpgm/paginationbackend/PaginationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devpgm.paginationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
